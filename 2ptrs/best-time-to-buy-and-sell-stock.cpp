8b53fa26d616ac668b2d277a0c4dc86abe2b6800
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2022-12-10 21:59:52 -0800

    Create best-time-to-buy-and-sell-stock.cpp

---
+++ 2ptrs/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,20 @@
+// 2 ptrs, right ptr keeps moving along the prices, left ptr updates to right ptr if right ptr price is lower
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int left = 0, right = 1;
+        int maxProfit = 0;
+
+        while(right < prices.size()) {
+            if (prices[left] < prices[right]) {
+                int currProfit = prices[right] - prices[left];
+                maxProfit = max(maxProfit, currProfit);
+            }
+            else {
+                left = right;
+            }
+            right += 1;
+        }
+        return maxProfit;
+    }
+};
