bcedecf0126b5e113158bb542dc8b9339698125b
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2022-12-10 21:58:48 -0800

    Update two-sum.cpp

--- umap/two-sum.cpp
+++ umap/two-sum.cpp
@@ -1,1 +1,14 @@
-
+// create umap where key is target sum to find & value is the index where that target sum is located
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map <int, int> umap;
+        for (int i = 0; i < nums.size(); i++) {
+            int targetSum = target - nums[i];
+            if (umap.find(targetSum) != umap.end())
+                return {umap[targetSum], i};
+            umap[nums[i]] = i;
+        }
+        return {-1, -1};
+    }
+};
