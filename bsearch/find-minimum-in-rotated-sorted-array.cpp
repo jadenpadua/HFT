80605de9a02c8d1831c4238016bf2e3daba39a20
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2022-12-11 21:48:17 -0800

    Update find-minimum-in-rotated-sorted-array.cpp

--- bsearch/find-minimum-in-rotated-sorted-array.cpp
+++ bsearch/find-minimum-in-rotated-sorted-array.cpp
@@ -1,3 +1,9 @@
+/**
+* @description: 
+* Step 1: We know that if the mid element is greater than the right element in bsearch, the pivot exists there, so cut down the already sorted half
+* Step 2: if its not greater, then pivot exists in left half, so we cut down the right half
+* Step 3: Repeat the process until left and right meet, then your left pointer will always be on the min element when we cut down to one element
+*/
 class Solution {
 public:
     int findMin(vector<int>& nums) {
