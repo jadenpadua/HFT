8a511525a54711574110878e6d88acd155f39941
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2022-12-24 19:31:30 -0800

    Create contains-duplicate-ii.cpp

---
+++ umap/contains-duplicate-ii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int> umap;
+        for(int i = 0; i < nums.size(); i++) {
+            if(umap.find(nums[i]) != umap.end() && abs(umap[nums[i]] - i) <= k) return true;
+            umap[nums[i]] = i;
+        }
+        return false;
+    }
+};
