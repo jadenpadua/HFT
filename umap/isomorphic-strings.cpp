88ea29a2abcbd9ddac1f1a3bb1bc33fe4d888cf9
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2023-01-11 21:12:01 -0800

    Create isomorphic-strings.cpp

---
+++ umap/isomorphic-strings.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int m1[256], m2[256];
+        for(int i = 0; i < 256; ++i) m1[i] = m2[i] = -1;
+
+        for(int i = 0; i < s.size(); ++i) {
+            if(m1[s[i]] != m2[t[i]]) return false;
+            m1[s[i]] = m2[t[i]] = i;
+        }
+        return true;
+    }
+};
