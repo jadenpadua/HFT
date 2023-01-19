a5c054aab4684a96f7024faca717692def3951eb
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2023-01-19 01:13:52 -0800

    Update add-binary.cpp

--- math/add-binary.cpp
+++ math/add-binary.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int p1 = a.size() - 1, p2 = b.size() - 1, carry {};
-        string res;
-
-        while(p1 >= 0 || p2 >= 0 || carry == 1) {
-            if(p1 >= 0) carry += a[p1--] - '0';
-            if(p2 >= 0) carry += b[p2--] - '0';
+        int carry {};
+        string res {};
+        int i = a.size() - 1, j = b.size() - 1;
+        while(i >= 0 || j >= 0 || carry) {
+            if(i >= 0) carry += a[i--] - '0';
+            if(j >= 0) carry += b[j--] - '0';
 
             res += (carry % 2) + '0';
             carry /= 2;
