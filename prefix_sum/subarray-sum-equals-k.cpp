6177aedebd3dc3754da780ba2f525a4747d9038f
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2023-01-19 01:13:26 -0800

    Update subarray-sum-equals-k.cpp

--- prefix_sum/subarray-sum-equals-k.cpp
+++ prefix_sum/subarray-sum-equals-k.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int> umap = {{0,1}};
-        int res {}, runningSum = 0;
+        unordered_map<int,int> umap {
+            {0,1},
+        };
+        int runningSum {};
+        int res {};
         for(int i = 0; i < nums.size(); ++i) {
             runningSum += nums[i];
-            int prefixSum = runningSum - k;
-            if(umap.find(prefixSum) != umap.end()) res += umap[prefixSum];
+            int prefSumToFind = runningSum - k;
+            if(umap.find(prefSumToFind) != umap.end()) {
+                res += umap[prefSumToFind];
+            }
             umap[runningSum]++;
         }
         return res;
