aa7d76e24b08e76b319f7a6e97e9673299d924a1
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2023-01-21 20:55:29 -0800

    Create sort-the-students-by-their-kth-score.cpp

---
+++ sorting/sort-the-students-by-their-kth-score.cpp
@@ -0,0 +1,10 @@
+class Solution {
+private:
+public:
+    vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
+        sort(score.begin(), score.end(), [&](vector<int> a,vector<int> b) {
+            return a[k] > b[k];
+        });
+        return score;
+    }
+};
