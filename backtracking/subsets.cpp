3c842764492c1cba74ccd3cfa3dd826c7dbe10d2
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2023-01-27 20:17:04 -0800

    Update subsets.cpp

--- backtracking/subsets.cpp
+++ backtracking/subsets.cpp
@@ -1,15 +1,18 @@
 class Solution {
+private:
+    void backtrack(vector<vector<int>>& res, vector<int>& currList, vector<int> nums, int ptr) {
+        res.push_back(currList);
+        for(int i = ptr; i < nums.size(); ++i) {
+            currList.push_back(nums[i]);
+            backtrack(res, currList, nums, i+1);
+            currList.pop_back();
+        }
+    }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> subs {{}};
-        for(int i = 0; i < nums.size(); ++i) {
-            int n = subs.size();
-            for(int j = 0; j < n; ++j) {
-                vector<int> newMember = subs[j];
-                newMember.push_back(nums[i]);
-                subs.push_back(newMember);
-            }
-        }
-        return subs;
+        vector<vector<int>> res {};
+        vector<int> currList {};
+        backtrack(res, currList, nums, 0);
+        return res;
     }
 };
